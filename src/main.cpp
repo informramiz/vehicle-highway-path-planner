1c4fe342527f46fd208ee68416069987d002a707
Ramiz Raja <informramiz@gmail.com>
2017-09-02 00:37:30 +0500

    refactor: pass state variables to lambda

--- src/main.cpp
+++ src/main.cpp
@@ -215,7 +215,7 @@ int main() {
   //define desired velocity
   double ref_velocity = 49.5; //mph
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  h.onMessage([&ref_velocity, &lane, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
