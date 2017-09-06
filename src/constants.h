6b27d5d768fd3301de626f11a5ad8afbb9ef8da7
Ramiz Raja <informramiz@gmail.com>
2017-09-06 14:32:04 +0500

    refactor: Update constants

--- src/constants.h
+++ src/constants.h
@@ -16,14 +16,14 @@ public:
   static const int N_SAMPLES = 10;
   static const vector<double> SIGMA_S;
   static const vector<double> SIGMA_D;
-  constexpr static const double SIGMA_T = 4.0;
+  constexpr static const double SIGMA_T = 4;
 
   constexpr static const double MAX_JERK = 10; // m/s^3
   constexpr static const double MAX_ACCELERATION = 10; // m/s^2
   constexpr static const double EXPECTED_JERK_IN_ONE_SEC = 2; //m/s^2
   constexpr static const double EXPECTED_ACCELERATION_IN_ONE_SEC = 1; // m/s
 
-  constexpr static const double SPEED_LIMIT = 30;
+  constexpr static const double SPEED_LIMIT = 50;
   constexpr static const double VEHICLE_RADIUS = 1.5; //model vehicle as circle to simplify collision detection
 };
 
